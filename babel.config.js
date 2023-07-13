9374c4f71cf7ee93ea92ce53836bf8f4e68d577a
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-13 15:23:54 -0500

    Configurando el proyecto

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@/controllers": "./src/controllers",
+            "@/models": "./src/models",
+            "@/services": "./src/services",
+            "@/views": "./src/views",
+            "@/atomic/element": "./atomic/element",
+            "@/atomic/component": "./atomic/component",
+            "@/atomic/design": "./atomic/design",
+          },
+        },
+      ],
+      ["nativewind/babel"],
+      [
+        "module:react-native-dotenv",
+        {
+         'moduleName':'@env',
+         'path':'.env',
+        }
+      ]
+    ],
   };
 };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./atomic/**/*.{js,jsx,ts,tsx}",
+    "./src/views/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
